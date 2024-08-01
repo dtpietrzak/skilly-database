d562fd9600190a94d579a0b15ec24cdc141ef5c6
dtpietrzak <dtpietrzak@gmail.com>
2024-07-31 21:10:43 -0500

    updated build scripts

--- build_prod.sh
+++ build_prod.sh
@@ -2,7 +2,7 @@
 
 lsof -i :7777 | grep LISTEN | awk '{print $2}' | xargs kill -9
 
-gcc -fsanitize=address -g -o build/database main.c $(find settings -name "*.c") $(find utils -name "*.c") $(find requests -name "*.c") $(find server -name "*.c") \
+clang -o build/database main.c $(find settings -name "*.c") $(find utils -name "*.c") $(find requests -name "*.c") $(find server -name "*.c") \
     -I./utils -I./requests -I./server \
     -I/opt/homebrew/opt/openssl/include \
     -L/opt/homebrew/opt/openssl/lib -lssl -lcrypto \
